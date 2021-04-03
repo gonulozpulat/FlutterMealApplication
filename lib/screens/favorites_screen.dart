2506ce87849db6b3b175a616dba979256fb498a2
gonulozpulat <gonulozpulat14@gmail.com>
2021-04-03 22:12:10 +0300

    flutter meal application added.

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:mealApplication/models/meal.dart';
+import '../widgets/meal_item.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('You have no favorites yet - strat adding some'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+            //removeItem: _removeMeal,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
